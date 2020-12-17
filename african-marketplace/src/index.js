7cb4f2c19e58afea6a7c41fbf224436ab6e87faf
harvey magana <heyharv2012@gmail.com>
2020-12-16 19:57:02 -0800

    Imported marketReducer into src/index.js, created reducer directory and reducer/marketReducers file and created the base logic in the marketReducer.

--- african-marketplace/src/index.js
+++ african-marketplace/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { marketReducer } from './reducers/marketReducers';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
