28e3c28919aa38e7aeb7ab44559cd8b59cd4494a
Samantha-Dutcher1986 <68967583+Samantha-Dutcher1986@users.noreply.github.com>
2020-12-17 00:19:38 -0600

    created login component and did some styling in app.css

---
+++ african-marketplace/src/Login.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+// import axios from 'axios';
+
+
+function Login(props) {
+    const userName = useFormInput('');
+    const password = useFormInput('');
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const handleLogin = () => {
+        setError(null);
+        setLoading(true);
+        /*axios.post('http://localhost:4000/users/signin', { username: username.value, password: password.value }).then(response => {
+            setLoading(false);
+            setUserSession(response.data.token, response.data.user);
+            props.history.push('/dashboard');
+        }).catch(error => {
+            setLoading(false);
+            if (error.response.status === 401) setError(error.response.data.message);
+            else setError("Something went wrong. Please try again later.");
+        });*/
+}
+    return (
+        <div>
+            Login<br />
+            <div>
+                Username<br />
+                <input type="text" {...userName} autoComplete="new-password" />
+            </div>
+            <div style={{ marginTop: 10 }}>
+                Password<br />
+                <input type="password" {...password} autoComplete="new-password" />
+            </div>
+            {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+            <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
+        </div>
+    );
+}
+
+const useFormInput = initialValue => {
+    const [value, setValue] = useState(initialValue);
+
+    const handleChange = e => {
+        setValue(e.target.value);
+    }
+    return{
+        value,
+        onChange: handleChange
+    }
+}
+
+export default Login;
