43375d403d6cef114e36452a38d30aa3a936307a
Erick Villegaz <64494158+evllz@users.noreply.github.com>
2020-12-17 18:17:38 -0800

    Revert "created login component and did some styling in app.css"

--- african-marketplace/src/index.js
+++ african-marketplace/src/index.js
@@ -7,7 +7,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
 const store = createStore(marketReducer)
 
 ReactDOM.render(
@@ -18,7 +17,6 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
