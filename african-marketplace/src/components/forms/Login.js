46b082dc03983440540d195a918b917e33a62c49
harvey magana <heyharv2012@gmail.com>
2020-12-20 09:46:57 -0800

    Styled the login to make it look nice, also fixed the navlinks so they go to the login, logout and ProductList.

--- african-marketplace/src/components/forms/Login.js
+++ african-marketplace/src/components/forms/Login.js
@@ -2,6 +2,31 @@ import React, { useState } from 'react';
 import { withRouter, Redirect } from "react-router-dom";
 import { connect } from 'react-redux';
 import { login } from '../../actions/marketActions';
+import styled from "styled-components";
+
+export const FormGroup = styled.form`
+	color: black;
+    display: block;
+	width: 300px;
+	margin: 50px auto;
+`;
+
+export const Label = styled.label`
+	margin-bottom: 0.5em;
+	color: palevioletred;
+    display: block;
+`;
+
+
+export const Input = styled.input`
+	padding: 0.5em;
+	color: palevioletred;
+	background: papayawhip;
+	border: none;
+	border-radius: 3px;
+	width: 100%;
+	margin-bottom: 0.5em;
+`;
 
 const Login = (props) => {
     console.log(props)
@@ -32,24 +57,24 @@ const Login = (props) => {
 
     return (
         <div>
+            <FormGroup onSubmit={handleSubmit}>
             <h1>Login component</h1>
-            <form onSubmit={handleSubmit}>
-                <input  
-                    placeholder='Username' 
-                    type="text"
-                    name="username"
-                    value={credentials.username || ''}
-                    onChange={handleChange}
-                />
-                <input 
-                    placeholder='Password' 
-                    type="password"
-                    name="password"
-                    value={credentials.password || ''}
-                    onChange={handleChange}
-                />
-                <button>Login</button>
-            </form>
+            <Input id="label" 
+                placeholder='Username' 
+                type="text"
+                name="username"
+                value={credentials.username || ''}
+                onChange={handleChange}
+            />
+            <Input 
+                placeholder='Password' 
+                type="password"
+                name="password"
+                value={credentials.password || ''}
+                onChange={handleChange}
+            />
+            <button>Login</button>
+            </FormGroup>
         </div>
     )
 }
