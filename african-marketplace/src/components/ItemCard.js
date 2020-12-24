271649738371cc5302c3fdf47a49106bcd046e65
unknown <erickvillegasecheverria@gmail.com>
2020-12-23 20:07:00 -0800

    added Market component with search

--- african-marketplace/src/components/ItemCard.js
+++ african-marketplace/src/components/ItemCard.js
@@ -38,23 +38,23 @@ const Button = styled.button`
 `;
 
 const ItemCard = (props) => {
-  console.log(props)
+  console.log(props);
   const handleClick = (e) => {
     e.preventDefault();
     alert("Item added!");
   };
   return (
     <Container>
-      <Title>{props.data.name}</Title>
+      <Title>{props.name}</Title>
       <div
         style={{
           display: "flex",
           flexDirection: "column",
           textAlign: "left",
-          margin: "0px 15px"
+          margin: "0px 15px",
         }}>
-        <Text>Value: {props.data.pantone_value}</Text>
-        <Text>Year: {props.data.year}</Text>
+        <Text>Value: {props.pantone_value}</Text>
+        <Text>Year: {props.year}</Text>
       </div>
 
       <Button onClick={handleClick}>Add Item</Button>
