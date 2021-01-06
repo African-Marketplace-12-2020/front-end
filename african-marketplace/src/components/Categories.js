871b698f1465a519463d368845b3135ad0539f96
harvey magana <heyharv2012@gmail.com>
2021-01-05 18:17:32 -0800

    Created the Categories file and built out the Categories component.

---
+++ african-marketplace/src/components/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Categories = (props) => {
+    return (
+        <div>
+            <h3>Categories component</h3>
+            {/** map the props category data into this component */}
+        </div>
+        
+    )
+}
+
+export default Categories;
